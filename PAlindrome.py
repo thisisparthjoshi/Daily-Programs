76a398e200beba9d57bf664586da84d72cc9dad4
thisisparthjoshi <71059599+thisisparthjoshi@users.noreply.github.com>
2020-11-02 12:48:40 +0530

    Delete PAlindrome.py

--- PAlindrome.py
+++
@@ -1,9 +0,0 @@
-# Program to check if a string is palindrome or not
-
-my_str = str(input("Enter the string: "))
-my_str = my_str.casefold()
-rev_str = reversed(my_str)
-if list(my_str) == list(rev_str):
-   print("The string is a palindrome.")
-else:
-   print("The string is not a palindrome.")
